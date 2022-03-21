ce66cd5d982911bcd0708012b089cbd2ff3b8aaa
Katherine Qin <katherine@tritonwear.com>
2022-03-21 18:59:52 -0400

    manual clean raw_1AB

--- data/clean.py
+++ data/clean.py
@@ -5,9 +5,10 @@ cleaning raw data set
 import pandas as pd
 import numpy as np
 
-fileName = "raw_coop1.csv"
+fileName = "raw_1AB.csv"
 fileExportName = fileName[4:]
 
 df = pd.read_csv('data/datasets/' + fileName)
+print(len(df.index))
 
 df.to_csv('data/datasets/' + fileExportName)
